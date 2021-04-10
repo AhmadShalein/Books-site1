d7c670b1452cbfc80d82122af24594abe4a58dd6
Ahmad Shalein <80862643+AhmadShalein@users.noreply.github.com>
2021-04-11 00:02:06 +0300

    Initial commit

---
+++ JavaScript/app.js
@@ -0,0 +1,15 @@
+'use strict';
+
+let userName = prompt('What\'s your name?');
+console.log(typeof userName);
+
+let userMajor = prompt("What\'s your major?");
+console.log(userMajor);
+
+let favWriter = prompt("Who\'s your favorite writer?");
+console.log(favWriter);
+
+let BookType = prompt('What kind of book is your favorite?');
+console.log(BookType);
+
+alert('Your name is ' + userName + ' and your major is ' + userMajor + ' your favourite writer is ' + favWriter + ' the kind of books you like is ' + BookType);
